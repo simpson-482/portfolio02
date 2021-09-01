d4514b64eae7ec32a6f9b774da6529285dfc3e37
Jared Simpson <66752595+simpson-482@users.noreply.github.com>
2021-09-01 13:42:06 -0400

    working on cleaning up code

--- Papers-School/script.js
+++ Papers-School/script.js
@@ -1,14 +1,14 @@
 
-var button = document.getElementById("togglebutton");
-var img1 = document.getElementById("paperrow1");
-var img2 = document.getElementById("paperrow2");
-var img3 = document.getElementById("paperrow3");
-var img4 = document.getElementById("paperrow4");
-var img5 = document.getElementById("paperrow5");
-var img6 = document.getElementById("paperrow6");
-var img7 = document.getElementById("paperrow7");
-var img8 = document.getElementById("paperrow8");
-var img9 = document.getElementById("paperrow9");
+var button = document.getElementById('togglebutton');
+var img1 = document.getElementById('paperrow1');
+var img2 = document.getElementById('paperrow2');
+var img3 = document.getElementById('paperrow3');
+var img4 = document.getElementById('paperrow4');
+var img5 = document.getElementById('paperrow5');
+var img6 = document.getElementById('paperrow6');
+var img7 = document.getElementById('paperrow7');
+var img8 = document.getElementById('paperrow8');
+var img9 = document.getElementById('paperrow9');
 
 function changeImage(){
     img1.setAttribute.src= "assets/images/Papers-Electric1.png";
