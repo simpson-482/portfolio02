f93d45583751034997352ee64604d83aa9ed1085
Jared Simpson <66752595+simpson-482@users.noreply.github.com>
2021-09-01 13:05:58 -0400

    working on image transition

---
+++ script.js
@@ -0,0 +1,23 @@
+var button = document.getElementById("toggle__button");
+var img1 = document.getElementById("paperrow1");
+var img2 = document.getElementById("paperrow2");
+var img3 = document.getElementById("paperrow3");
+var img4 = document.getElementById("paperrow4");
+var img5 = document.getElementById("paperrow5");
+var img6 = document.getElementById("paperrow6");
+var img7 = document.getElementById("paperrow7");
+var img8 = document.getElementById("paperrow8");
+var img9 = document.getElementById("paperrow9");
+
+
+button.addEventListener("click", function changeImage(){
+    img1.attribute.src= "assets/images/Papers-Electric1.png";
+    img2.attribute.src= "assets/images/Papers-Hawaii2.png";
+    img3.attribute.src= "assets/images/Papers-Corals2.png";
+    img4.attribute.src= "assets/images/Papers-FBall2.png";
+    img5.attribute.src= "assets/images/Papers-VNWar2.png";
+    img6.attribute.src= "assets/images/Papers-SteelTown2.png";
+    img7.attribute.src= "assets/images/Papers-ClassMyth1.2.png";
+    img8.attribute.src= "assets/images/Papers-ClassMyth2.2.png";
+    img9.attribute.src= "assets/images/Papers-ClassMyth3.2.png";
+});
