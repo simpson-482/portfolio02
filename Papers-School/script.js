5eec0bc5a4a6bb66d1cbd78ad6ad55e52c4663d3
Jared Simpson <66752595+simpson-482@users.noreply.github.com>
2021-09-15 18:51:49 -0400

    added new files

--- Papers-School/script.js
+++ Papers-School/script.js
@@ -34,8 +34,38 @@
 
 // Attempt 2
 
-var togglebutton = document.getElementById('togglebutton');
+// var imgSwitch = document.getElementById('first-image');
+// var togglebutton = document.getElementById('togglebutton');
 
-togglebutton.addEventListener('click', function(togglebutton){
-    togglebutton.target.classList.toggle('divimage1');
-});
+// togglebutton.addEventListener('click', function(imgSwitch){
+//     imgSwitch.target.classList.toggle('divImage');
+// });
+
+// Attempt 3
+
+var button1 = document.getElementById('togglebutton');
+var img1 = document.getElementById('paperrow1');
+var img2 = document.getElementById('paperrow2');
+var img3 = document.getElementById('paperrow3');
+var img4 = document.getElementById('paperrow4');
+var img5 = document.getElementById('paperrow5');
+var img6 = document.getElementById('paperrow6');
+var img7 = document.getElementById('paperrow7');
+var img8 = document.getElementById('paperrow8');
+var img9 = document.getElementById('paperrow9');
+
+
+
+function changeImage(){
+    img1.src = '../assets/images/Papers-Electric1.png';
+    img2.src = '../assets/images/Papers-Hawaii2.png';
+    img3.src = '../assets/images/Papers-Corals2.png';
+    img4.src = '../assets/images/Papers-FBall2.png';
+    img5.src = '../assets/images/Papers-VNWar2.png';
+    img6.src = '../assets/images/Papers-SteelTown2.png';
+    img7.src = '../assets/images/Papers-ClassMyth1.2.png';
+    img8.src = '../assets/images/Papers-ClassMyth2.2.png';
+    img9.src = '../assets/images/Papers-ClassMyth3.2.png';
+}
+
+button1.addEventListener('click', changeImage());
